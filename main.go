1aac999b2874f75fdf15344a8e32d3d49094591c
Jansen <jsd.alcantara@gmail.com>
2025-08-06 11:08:36 -0400

    jansen: english

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ func main() {
 	port := 8080
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		log.Printf(">> Nova request - %s %s\n", r.Method, r.URL)
+		log.Printf(">> New request - %s %s\n", r.Method, r.URL)
 		io.WriteString(w, "Hello!\n")
 	}
 
 	http.HandleFunc("/", h1)
 
-	log.Printf(">> Server rodando na porta :%d\n", port)
+	log.Printf(">> Server runnning at :%d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
